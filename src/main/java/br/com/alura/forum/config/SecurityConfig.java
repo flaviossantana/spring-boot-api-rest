a1abb5c4d81a488cbc3625b20785a986833f21f9
flavio.santana <9090flavio@gmail.com>
2022-08-31 08:46:45 -0300

    desv: restringindo o acesso aos endpoints privados com formlogin

--- src/main/java/br/com/alura/forum/config/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
     }
 }
