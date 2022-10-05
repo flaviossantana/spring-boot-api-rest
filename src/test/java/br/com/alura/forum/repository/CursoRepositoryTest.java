d241167ab4213af215d4a0aa2be207022478efc2
flavio.santana <9090flavio@gmail.com>
2022-10-05 11:21:28 -0300

    desv: testando o repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,22 +5,36 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoRepositoryTest {
+
+    private static final String CURSO_HTML_5 = "HTML 5";
 
     @Autowired
     private CursoRepository repository;
 
+    private void criarCursoHtml5(){
+        this.repository.save(new Curso(CURSO_HTML_5, "Programação"));
+    }
+
     @Test
     public void deveriaBuscarCursoPorNome() {
-        String nomeCurso = "HTML 5";
-        Curso curso = this.repository.findByNome(nomeCurso);
+
+        criarCursoHtml5();
+
+        Curso curso = this.repository.findByNome(CURSO_HTML_5);
         Assert.assertNotNull(curso);
-        Assert.assertEquals(curso.getNome(), nomeCurso);
+        Assert.assertEquals(CURSO_HTML_5, curso.getNome());
+        Assert.assertNotNull(curso.getCategoria());
+        Assert.assertNotNull(curso.getId());
     }
 
     @Test
